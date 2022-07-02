fcb9aa63c183a94d6609836235279b05873be1b8
Mateusz Ostrowski <mateusz.ostrowski@wrenkitchens.com>
2022-07-02 23:18:40 +0100

    added methods to field storage

--- src/app/core/storages/field-storage.service.ts
+++ src/app/core/storages/field-storage.service.ts
@@ -1,18 +1,71 @@
 import { Injectable } from '@angular/core';
 import { Field } from '../models/field/field-model';
+import { Subject } from 'rxjs';
+import { PublicConstantsService } from '../constants/public-constants.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class FieldStorageService {
+  private fields: Field[] = [];
+  private fieldNames: string[] = [];
+  private fieldNamesSubject: Subject<string[]> = new Subject<string[]>();
+  private fieldsSubject: Subject<string[]> = new Subject<string[]>();
 
-  constructor() { }
+  constructor(private publicConstants: PublicConstantsService) { }
 
-  addFieldNameToArray(fieldName: string) {
-
+  addFieldNameToArray(newFieldName: string) {
+    this.fieldNames.forEach((fieldName) => {
+      if (fieldName === newFieldName) {
+        throw new Error(this.publicConstants.FIELD_NAME_EXISTS_EXCEPTION);
+      }
+    })
+    this.fieldNames.push(newFieldName);
+    this.fieldNamesSubject.next(this.fieldNames);
   }
 
   addFieldToArray(newField: Field) {
+    this.fields.push(newField);
+    this.fieldsSubject.next(this.fieldNames);
+  }
 
+  public getFieldByName(name: string): Field {
+    if (!name) {
+      throw new Error(this.publicConstants.PLEASE_SELECT_THE_FIELD);
+    }
+    let fetchedField: Field | null = null;
+    for (let field of this.fields) {
+      if (field.getFieldName() === name) {
+        fetchedField = field;
+        break;
+      }
+    }
+    if (!!fetchedField) {
+      return fetchedField;
+    }
+    throw new Error(this.publicConstants.FIELD_NOT_FOUND_EXCEPTION);
+  }
+
+  public getFieldByArrayIndex(index: number): Field | null {
+    if (this.fields.length < 1) {
+      return null;
+    }
+    return this.fields[index];
+  }
+
+  public getFields(): Field[] {
+    return this.fields;
+  }
+
+  public getFieldNames(): string[] {
+    return this.fieldNames;
+  }
+
+  public getFieldNamesSubject(): Subject<string[]> {
+    return this.fieldNamesSubject;
+  }
+
+  public getFieldsSubject(): Subject<string[]> {
+    return this.fieldsSubject;
   }
 }
