f994cfd1aa3ec28546e2eba7cd7ecdba9e77f077
Mateusz Ostrowski <mateusz.ostrowski@wrenkitchens.com>
2022-07-06 01:22:33 +0100

    removed unused constants

--- src/app/core/constants/public-constants.service.ts
+++ src/app/core/constants/public-constants.service.ts
@@ -8,9 +8,6 @@ export class PublicConstantsService {
   constructor() {
   }
 
-  //Field
-  readonly SELECTED_FIELD_WHEN_NO_FIELDS_CREATED = 'No fields created';
-
   //Sheep
   readonly LAMB_NEW_BORN_DEFAULT_NAME = 'Little Bob';
   readonly SHEEP_CREATED_MESSAGE = 'Sheep created successfully!'
@@ -20,6 +17,5 @@ export class PublicConstantsService {
   readonly FIELD_NAME_EXISTS_EXCEPTION = 'Field name already exists! Try a different name.';
   readonly FIELD_NOT_FOUND_EXCEPTION = 'Field name do not exists!';
   readonly PLEASE_SELECT_THE_FIELD = 'Please select the field!';
-  readonly PLEASE_NAME_THE_FIELD = 'Please name the field!';
   readonly NO_MORE_SHEEP_TO_BRAND = 'No more sheep to brand';
 }
