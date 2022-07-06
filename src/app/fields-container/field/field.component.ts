b88bbe4b6c884aa8ebeaac6a1caee32a7e29772a
Mateusz Ostrowski <mateusz.ostrowski@wrenkitchens.com>
2022-07-06 01:22:02 +0100

    added alert when no more sheep to brand

--- src/app/fields-container/field/field.component.ts
+++ src/app/fields-container/field/field.component.ts
@@ -2,6 +2,8 @@ import { Component, Input } from '@angular/core';
 import { Field } from '../../core/models/field/field-model';
 import { SheepFactoryService } from '../../core/services/sheep-factory.service';
 import { RowMatingService } from '../../core/services/row-mating.service';
+import { ErrorHandlerService } from '../../core/services/error-handler.service';
+import { PublicConstantsService } from '../../core/constants/public-constants.service';
 
 @Component({
   selector: 'app-field',
@@ -12,13 +14,17 @@ export class FieldComponent {
   @Input() field!: Field;
 
   constructor(private sheepFactory: SheepFactoryService,
-              private rowMatingService: RowMatingService) {
+              private rowMatingService: RowMatingService,
+              private errorHandler: ErrorHandlerService,
+              private publicConstants: PublicConstantsService) {
   }
 
   public onBrandRandom(): void {
     const randomSheep = this.field.getRandomUnbrandedSheep();
     if (!!randomSheep) {
       this.rowMatingService.brandSheep(randomSheep);
+    } else {
+      this.errorHandler.handleError(this.publicConstants.NO_MORE_SHEEP_TO_BRAND);
     }
   }
 }
