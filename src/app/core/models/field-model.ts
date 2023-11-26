a376d90f71b905317d19cc51c436efa882c1a5ff
Mateusz Ostrowski <mateusz.ostrowski@blackdotsolutions.com>
2023-11-26 16:23:29 +0000

    refactoring

--- src/app/core/models/field-model.ts
+++ src/app/core/models/field-model.ts
@@ -33,6 +33,28 @@ export class Field {
     sheep.setRowIndexTheSheepIsAssignedTo(this, indexOfNewRow);
   }
 
+  getRandomUnbrandedSheep(): AbstractSheep | null {
+    const allUnbrandedSheep = this.sheepInside.filter((sheep) => !(sheep.isBranded || AbstractSheep.isLamb(sheep)));
+    return allUnbrandedSheep.length > 0 ? allUnbrandedSheep[Math.floor(Math.random() * allUnbrandedSheep.length)] : null;
+  }
+
+  removeOneLamb(): void {
+    this.allLambSheep.pop();
+
+    let indexToDelete = -1;
+    this.allSheep.forEach((sheep, index) => {
+      if (sheep instanceof LambSheep) {
+        indexToDelete = index;
+      }
+    });
+
+    if (indexToDelete >= 0) {
+      this.allSheep.splice(indexToDelete, 1);
+    } else {
+      throw new Error('Lamb has escaped from growing up, I guess it will be happy forever...');
+    }
+  }
+
   get numberOfRows(): number {
     return this.biggerArrayOfSheep.length;
   }
@@ -55,28 +77,6 @@ export class Field {
 
   get allFemaleSheep(): FemaleSheep[] {
     return this.femaleSheepInside;
-  }
-
-  getRandomUnbrandedSheep(): AbstractSheep | null {
-    const allUnbrandedSheep = this.sheepInside.filter((sheep) => !(sheep.isBranded || AbstractSheep.isLamb(sheep)));
-    return allUnbrandedSheep.length > 0 ? allUnbrandedSheep[Math.floor(Math.random() * allUnbrandedSheep.length)] : null;
-  }
-
-  removeOneLamb(): void {
-    this.allLambSheep.pop();
-
-    let indexToDelete = -1;
-    this.allSheep.forEach((sheep, index) => {
-      if (sheep instanceof LambSheep) {
-        indexToDelete = index;
-      }
-    });
-
-    if (indexToDelete >= 0) {
-      this.allSheep.splice(indexToDelete, 1);
-    } else {
-      throw new Error('Lamb has escaped from growing up, I guess it will be happy forever...');
-    }
   }
 
   get allLambSheep(): LambSheep[] {
